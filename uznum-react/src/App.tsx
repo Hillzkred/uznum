f09a297641670202aad1d77d0970676a1cbdadae
Alexis Almacin <17034756+Hillzkred@users.noreply.github.com>
2023-01-02 22:41:52 -0600

    Finished setting up tanstack router

--- uznum-react/src/App.tsx
+++ uznum-react/src/App.tsx
@@ -1,7 +1,28 @@
+import {
+  createReactRouter,
+  createRouteConfig,
+  RouterProvider,
+} from '@tanstack/react-router';
+import Router from './components/Router';
+import About from './pages/About';
+import Home from './pages/Home';
+
+const rootRoute = createRouteConfig({
+  component: Router,
+});
+
+const indexRoute = rootRoute.createRoute({ path: '/', component: Home });
+
+const aboutRoute = rootRoute.createRoute({ path: '/about', component: About });
+
+const routeConfig = rootRoute.addChildren([indexRoute, aboutRoute]);
+
+const router = createReactRouter({ routeConfig });
+
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <RouterProvider router={router} />
     </div>
   );
 }
