7e7f93e729ea3edb4319289276d99f280169868e
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2023-02-06 07:22:19 -0600

    Add sample registration page

--- uznum-react/src/pages/RegistrationPage.tsx
+++ uznum-react/src/pages/RegistrationPage.tsx
@@ -1,39 +1,10 @@
-import {FormEvent, useState} from "react";
+import {RegistrationForm} from "./registration/RegistrationForm";
 
 export function RegistrationPage() {
-
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-
-    const registrationHandler = (e: FormEvent) => {
-        e.preventDefault();
-        fetch('http://localhost:3001/register', {
-            method: 'POST',
-            body: JSON.stringify({
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        })
-            .then(response => response.json())
-            .then(data => console.log(data))
-            .catch(error => console.error(error));
-    }
-
-    return <div>
-        <h1>Registration Page</h1>
-
-        <form onSubmit={registrationHandler}>
-            <div>
-
-                <label>Username</label>
-                <input type="text" placeholder="username"/>
-            </div>
-            <div>
-                <label>Password</label>
-                <input type="password" placeholder="password"/>
-            </div>
-            <div>
-                <button type="submit">Register</button>
-            </div>
-        </form>
-    </div>
+    return (
+        <div>
+            <h1>Registration</h1>
+            <RegistrationForm />
+        </div>
+    );
 }
